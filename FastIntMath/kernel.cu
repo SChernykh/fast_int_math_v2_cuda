d5843dd40da246e981f4ce570bb690f840ae078e
SChernykh <sergey.v.chernykh@gmail.com>
2018-11-15 10:42:10 +0100

    More benchmarking and testing stuff

--- FastIntMath/kernel.cu
+++ FastIntMath/kernel.cu
@@ -11,6 +11,21 @@ constexpr size_t TEST_SQRT_STEP = 1 << 26;
 constexpr size_t TEST_DIV_STEP = 1 << 21;
 constexpr int NUMBERS_PER_DIVISOR = 256;
 
+// Run "generate_ptx.bat" and search for "fast_div_heavy BEGIN" in kernel.ptx to look at generated PTX assembly
+__global__ void DummyFastDivHeavyPTX(const int64_t* _a, const int32_t* _b, int64_t* _result)
+{
+	const int index = blockIdx.x * blockDim.x + threadIdx.x;
+	const int64_t a = _a[index];
+	const int32_t b = _b[index];
+	int64_t result;
+
+	asm("// fast_div_heavy BEGIN");
+	result = fast_div_heavy(a, b);
+	asm("// fast_div_heavy END");
+
+	_result[index] = result;
+}
+
 // Run "generate_ptx.bat" and search for "fast_div_v2 BEGIN" in kernel.ptx to look at generated PTX assembly
 __global__ void DummyFastDivPTX(const uint64_t* _a, const uint32_t* _b, uint64_t* _result)
 {
@@ -40,6 +55,27 @@ __global__ void DummyFastSqrtPTX(const uint64_t* _a, uint32_t* _result)
 	_result[index] = result;
 }
 
+__global__ void GetReciprocalTest(const uint32_t base, uint64_t* err_value)
+{
+	uint32_t b = base + blockIdx.x;
+	if (b == 0) b = 1;
+
+	const uint64_t r = get_reciprocal64(b);
+	uint64_t r0 = uint64_t(-1) / b;
+	double r1 = (uint64_t(-1) % b) + 1;
+	r1 /= b;
+
+	const double error = int64_t(r - r0) - r1;
+
+	if (((error <= -0.876) || (error >= 0.876)) && (atomicAdd((uint32_t*)err_value, 1) == 0))
+	{
+		err_value[1] = b;
+		err_value[2] = r;
+		err_value[3] = r0;
+		err_value[4] = *((uint64_t*)&error);
+	}
+}
+
 __global__ void FastDivTest(const uint64_t* _a, const uint32_t base, uint64_t* err_value)
 {
 	const uint64_t a = _a[threadIdx.x];
@@ -57,6 +93,40 @@ __global__ void FastDivTest(const uint64_t* _a, const uint32_t base, uint64_t* e
 	}
 }
 
+__global__ void FastDivHeavyTest(const int64_t* _a, const int32_t base, int64_t* err_value)
+{
+	const int64_t a = _a[threadIdx.x];
+	int32_t b = base + blockIdx.x;
+	if (b < 1) b = 1;
+
+	const int64_t fast_div_heavy_result = fast_div_heavy(a, b);
+	const int64_t correct_result = a / b;
+	if ((fast_div_heavy_result != correct_result) && (atomicAdd((uint32_t*)err_value, 1) == 0))
+	{
+		err_value[1] = a;
+		err_value[2] = b;
+		err_value[3] = fast_div_heavy_result;
+		err_value[4] = correct_result;
+	}
+}
+
+__global__ void FastDivHeavyOldTest(const int64_t* _a, const int32_t base, int64_t* err_value)
+{
+	const int64_t a = _a[threadIdx.x];
+	int32_t b = base + blockIdx.x;
+	if (b < 2) b = 2;
+
+	const int64_t fast_div_heavy_result = fast_div_heavy_old(a, b);
+	const int64_t correct_result = a / b;
+	if ((fast_div_heavy_result != correct_result) && (atomicAdd((uint32_t*)err_value, 1) == 0))
+	{
+		err_value[1] = a;
+		err_value[2] = b;
+		err_value[3] = fast_div_heavy_result;
+		err_value[4] = correct_result;
+	}
+}
+
 __global__ void FastDivBench(const uint64_t* _a, const uint32_t base, uint64_t* err_value)
 {
 	const uint64_t a = _a[threadIdx.x];
@@ -65,6 +135,44 @@ __global__ void FastDivBench(const uint64_t* _a, const uint32_t base, uint64_t* 
 
 	const uint64_t fast_div_result = fast_div_v2(a, b);
 	if (fast_div_result == 1)
+	{
+		err_value[0] = 0;
+	}
+}
+
+__global__ void FastDivHeavyBench(const int64_t* _a, const int32_t base, int64_t* err_value)
+{
+	const int64_t a = _a[threadIdx.x];
+	int32_t b = base + blockIdx.x;
+	if (b < 2) b = 2;
+
+	const int64_t fast_div_heavy_result = fast_div_heavy(a, b);
+	if (fast_div_heavy_result == 1)
+	{
+		err_value[0] = 0;
+	}
+}
+
+__global__ void FastDivHeavyOldBench(const int64_t* _a, const int32_t base, int64_t* err_value)
+{
+	const int64_t a = _a[threadIdx.x];
+	int32_t b = base + blockIdx.x;
+	if (b < 2) b = 2;
+
+	const int64_t fast_div_heavy_result = fast_div_heavy_old(a, b);
+	if (fast_div_heavy_result == 1)
+	{
+		err_value[0] = 0;
+	}
+}
+
+__global__ void NaiveDivHeavyBench(const int64_t* _a, const int32_t base, int64_t* err_value)
+{
+	const int64_t a = _a[threadIdx.x];
+	int32_t b = base + blockIdx.x;
+	if (b < 2) b = 2;
+
+	if (a / b == 1)
 	{
 		err_value[0] = 0;
 	}
@@ -155,6 +263,206 @@ cudaError_t TestIntMath()
 		goto Error;
 	}
 
+	printf("Benchmarking fast_div_heavy (all divisors, %d numbers per divisor)\n", NUMBERS_PER_DIVISOR);
+	LARGE_INTEGER f;
+	QueryPerformanceFrequency(&f);
+	dt = 0;
+	for (uint64_t base = 0; base < 0x80000000ULL; base += TEST_DIV_STEP * 16)
+	{
+		printf("%.1f%% done\r", base * 100.0 / 0x80000000ULL);
+
+		LARGE_INTEGER t1, t2;
+		QueryPerformanceCounter(&t1);
+
+		FastDivHeavyBench<<<TEST_DIV_STEP * 16, NUMBERS_PER_DIVISOR>>>((int64_t*)dev_a, (int32_t)(base), (int64_t*)dev_err_value);
+
+		cudaStatus = cudaGetLastError();
+		if (cudaStatus != cudaSuccess)
+		{
+			fprintf(stderr, "FastDivHeavyBench launch failed: %s\n", cudaGetErrorString(cudaStatus));
+			goto Error;
+		}
+
+		cudaStatus = cudaDeviceSynchronize();
+		if (cudaStatus != cudaSuccess)
+		{
+			fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+			goto Error;
+		}
+
+		QueryPerformanceCounter(&t2);
+		dt += t2.QuadPart - t1.QuadPart;
+	}
+	printf("100.0%% done, %.3f seconds\n", static_cast<double>(dt) / f.QuadPart);
+
+	printf("Benchmarking fast_div_heavy_old (all divisors, %d numbers per divisor)\n", NUMBERS_PER_DIVISOR);
+	dt = 0;
+	for (uint64_t base = 0; base < 0x80000000ULL; base += TEST_DIV_STEP * 16)
+	{
+		printf("%.1f%% done\r", base * 100.0 / 0x80000000ULL);
+
+		LARGE_INTEGER t1, t2;
+		QueryPerformanceCounter(&t1);
+
+		FastDivHeavyOldBench<<<TEST_DIV_STEP * 16, NUMBERS_PER_DIVISOR>>> ((int64_t*)dev_a, (int32_t)(base), (int64_t*)dev_err_value);
+
+		cudaStatus = cudaGetLastError();
+		if (cudaStatus != cudaSuccess)
+		{
+			fprintf(stderr, "FastDivHeavyOldBench launch failed: %s\n", cudaGetErrorString(cudaStatus));
+			goto Error;
+		}
+
+		cudaStatus = cudaDeviceSynchronize();
+		if (cudaStatus != cudaSuccess)
+		{
+			fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+			goto Error;
+		}
+
+		QueryPerformanceCounter(&t2);
+		dt += t2.QuadPart - t1.QuadPart;
+	}
+	printf("100.0%% done, %.3f seconds\n", static_cast<double>(dt) / f.QuadPart);
+
+	printf("Benchmarking naive div_heavy (all divisors, %d numbers per divisor)\n", NUMBERS_PER_DIVISOR);
+	dt = 0;
+	for (uint64_t base = 0; base < 0x80000000ULL; base += TEST_DIV_STEP * 16)
+	{
+		printf("%.1f%% done\r", base * 100.0 / 0x80000000ULL);
+
+		LARGE_INTEGER t1, t2;
+		QueryPerformanceCounter(&t1);
+
+		NaiveDivHeavyBench<<<TEST_DIV_STEP * 16, NUMBERS_PER_DIVISOR>>>((int64_t*)dev_a, (int32_t)(base), (int64_t*)dev_err_value);
+
+		cudaStatus = cudaGetLastError();
+		if (cudaStatus != cudaSuccess)
+		{
+			fprintf(stderr, "NaiveDivHeavyBench launch failed: %s\n", cudaGetErrorString(cudaStatus));
+			goto Error;
+		}
+
+		cudaStatus = cudaDeviceSynchronize();
+		if (cudaStatus != cudaSuccess)
+		{
+			fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+			goto Error;
+		}
+
+		QueryPerformanceCounter(&t2);
+		dt += t2.QuadPart - t1.QuadPart;
+	}
+	printf("100.0%% done, %.3f seconds\n", static_cast<double>(dt) / f.QuadPart);
+
+	printf("Testing fast_div_heavy (all divisors, %d numbers per divisor)\n", NUMBERS_PER_DIVISOR);
+	for (uint64_t base = 0; base < 0x80000000ULL; base += TEST_DIV_STEP)
+	{
+		printf("%.1f%% done\r", base * 100.0 / 0x80000000ULL);
+
+		FastDivHeavyTest<<<TEST_DIV_STEP, NUMBERS_PER_DIVISOR>>>((int64_t*)dev_a, (int32_t)(base), (int64_t*)dev_err_value);
+
+		cudaStatus = cudaGetLastError();
+		if (cudaStatus != cudaSuccess)
+		{
+			fprintf(stderr, "FastDivHeavyTest launch failed: %s\n", cudaGetErrorString(cudaStatus));
+			goto Error;
+		}
+
+		cudaStatus = cudaDeviceSynchronize();
+		if (cudaStatus != cudaSuccess)
+		{
+			fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+			goto Error;
+		}
+
+		uint64_t err_value[5] = {};
+		cudaStatus = cudaMemcpy(err_value, dev_err_value, sizeof(uint64_t) * 5, cudaMemcpyDeviceToHost);
+		if (cudaStatus != cudaSuccess)
+		{
+			fprintf(stderr, "cudaMemcpy failed!");
+			goto Error;
+		}
+		if (err_value[0] != 0)
+		{
+			printf("\nError:\na=%lld\nb=%lld\nfast_div_heavy result=%lld\ncorrect result=%lld\n", err_value[1], err_value[2], err_value[3], err_value[4]);
+			goto Error;
+		}
+	}
+	printf("100.0%% done\n");
+
+	printf("Testing fast_div_heavy_old (all divisors, %d numbers per divisor)\n", NUMBERS_PER_DIVISOR);
+	for (uint64_t base = 0; base < 0x80000000ULL; base += TEST_DIV_STEP)
+	{
+		printf("%.1f%% done\r", base * 100.0 / 0x80000000ULL);
+
+		FastDivHeavyOldTest<<<TEST_DIV_STEP, NUMBERS_PER_DIVISOR>>>((int64_t*)dev_a, (int32_t)(base), (int64_t*)dev_err_value);
+
+		cudaStatus = cudaGetLastError();
+		if (cudaStatus != cudaSuccess)
+		{
+			fprintf(stderr, "FastDivHeavyOldTest launch failed: %s\n", cudaGetErrorString(cudaStatus));
+			goto Error;
+		}
+
+		cudaStatus = cudaDeviceSynchronize();
+		if (cudaStatus != cudaSuccess)
+		{
+			fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+			goto Error;
+		}
+
+		uint64_t err_value[5] = {};
+		cudaStatus = cudaMemcpy(err_value, dev_err_value, sizeof(uint64_t) * 5, cudaMemcpyDeviceToHost);
+		if (cudaStatus != cudaSuccess)
+		{
+			fprintf(stderr, "cudaMemcpy failed!");
+			goto Error;
+		}
+		if (err_value[0] != 0)
+		{
+			printf("\nError:\na=%lld\nb=%lld\nfast_div_heavy result=%lld\ncorrect result=%lld\n", err_value[1], err_value[2], err_value[3], err_value[4]);
+			goto Error;
+		}
+	}
+	printf("100.0%% done\n");
+
+	printf("Testing get_reciprocal64 (all divisors)\n");
+	for (uint64_t base = 0; base < 0x100000000ULL; base += TEST_DIV_STEP)
+	{
+		printf("%.1f%% done\r", base * 100.0 / 0x100000000ULL);
+
+		GetReciprocalTest<<<TEST_DIV_STEP, 1>>>((uint32_t)(base), dev_err_value);
+
+		cudaStatus = cudaGetLastError();
+		if (cudaStatus != cudaSuccess)
+		{
+			fprintf(stderr, "GetReciprocalTest launch failed: %s\n", cudaGetErrorString(cudaStatus));
+			goto Error;
+		}
+
+		cudaStatus = cudaDeviceSynchronize();
+		if (cudaStatus != cudaSuccess)
+		{
+			fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+			goto Error;
+		}
+
+		uint64_t err_value[5] = {};
+		cudaStatus = cudaMemcpy(err_value, dev_err_value, sizeof(uint64_t) * 5, cudaMemcpyDeviceToHost);
+		if (cudaStatus != cudaSuccess)
+		{
+			fprintf(stderr, "cudaMemcpy failed!");
+			goto Error;
+		}
+		if (err_value[0] != 0)
+		{
+			printf("\nError:\nb=%llu\nr1=%llx\nr2=%llx\ndelta=%.6f", err_value[1], err_value[2], err_value[3], err_value[4]);
+			goto Error;
+		}
+	}
+	printf("100.0%% done\n");
+
 	printf("Testing fast_sqrt_v2 (all edge cases)\n");
 	for (uint32_t base = 0; base <= 1779033703; base += TEST_SQRT_STEP)
 	{
@@ -191,8 +499,6 @@ cudaError_t TestIntMath()
 	}
 
 	printf("Benchmarking fast_div_v2 (all divisors, %d numbers per divisor)\n", NUMBERS_PER_DIVISOR);
-	LARGE_INTEGER f;
-	QueryPerformanceFrequency(&f);
 	dt = 0;
 	for (uint64_t base = 0x80000000ULL; base < 0x100000000ULL; base += TEST_DIV_STEP * 16)
 	{
